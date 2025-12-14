e61d0d0f5ce6d68b8e4c69fad7a0aff34642cf0c
Abrar <rmd394195@gmial.comn>
2025-12-14 09:10:36 +0600

    2nd commit

---
+++ problem2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    char address[]="Rangpure,Kurigram,Bhurungamari";
+    printf("%s",address);
+    return 0;
+}
---
+++ problem3.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    char collage_name[]="Image polytechnic institute Rangpure";
+    printf("%s",collage_name);
+    return 0;
+}
