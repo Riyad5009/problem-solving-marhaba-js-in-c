458877cf010724847c7b372ce06dab624cfe6d08
Abrar <rmd394195@gmial.comn>
2025-12-15 09:55:06 +0600

    5th commit

---
+++ 2nd chapter/3rd chapter/problem1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main(){
+    char number1[]="20";
+    int number2=10;
+    int st=atoi(number1);
+    int sum=st+number2;
+    printf("%d",sum);
+    return 0;
+}
---
+++ 2nd chapter/cp3problem2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    char number[]="3.14159";
+    double ft=atoi(number);
+    printf("%lf",ft);
+    return 0;
+}
